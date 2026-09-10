e695de0d3f7c512fa7e6e196cd63553c31e172bb
adityasemalti <adityasemalti535@gmail.com>
2026-09-10 16:06:32 +0530

    okkk

--- app/(protected)/dashboard/Dashboard-Components/DashboardSearch.tsx
+++ app/(protected)/dashboard/Dashboard-Components/DashboardSearch.tsx
@@ -182,7 +182,7 @@ export default function DashboardSearch() {
                                                             key={person._id}
                                                             onClick={() =>
                                                                 router.push(
-                                                                    `/profile/${person._id}`
+                                                                    `/user-profile/${person._id}`
                                                                 )
                                                             }
                                                             className="flex w-full items-center gap-3 rounded-xl p-2.5 text-left transition hover:bg-zinc-50"
